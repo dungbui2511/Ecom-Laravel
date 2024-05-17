d05a70321c1dbe27b0df1deb1b6e27e76c23b17d
dungbui2511 <binzno2x@gmail.com>
2024-05-17 20:00:33 +0700

    add product

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -12,13 +12,26 @@
     <div class="col-md-12">
         <div class="panel panel-default">
             <div class="panel-heading">
+               <div class="row">
+                <div class="col-md-6">
                 All Categories
+                </div>
+                <div class="col-md-6">
+                    <a href="{{route('admin.addcategory')}}" class="btn btn-success pull-right">Add New</a>
+                </div>
+               </div>
             </div>
+            
             <div class="panel-body">
+                @if(Session::has('message'))
+                <div class="alert alert-success" role="alert">
+                    {{Session::get('message')}}
+                </div>
+                @endif
                 <table class="table table-striped">
                     <thead>
-                        <tr>Id</tr>
-                        <th>Category</th>
+                        <th>Id</th>
+                        <th>Category Name</th>
                         <th>Slug</th>
                         <th>Action</th>
                     </thead>
@@ -28,11 +41,17 @@
                             <td>{{$category->id}}</td>
                             <td>{{$category->name}}</td>
                             <td>{{$category->slug}}</td>
+                            <td>
+                                <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}"><i class="fa fa-edit fa-2x"></i></a>
+                                <a href="#" wire:click.prevent="deleteCategory({{$category->id}})" style="margin-left:10px;"><i class="fa fa-times fa-2x"></i></a>
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
                 </table>
+                {{$categories->links()}}
             </div>
+
         </div>
     </div>
    </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminAddCategoryComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\CheckoutComponent;
@@ -39,6 +43,10 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () {
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add',AdminAddCategoryComponent::class)->name('admin.addcategory');
+    Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+    Route::get('/admin/products/add', AdminAddProductComponent::class)->name('admin.addproduct');
 });
 
 Route::middleware([
