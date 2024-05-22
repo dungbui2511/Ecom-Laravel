7cfb517c945cd19e30a39480a7b623a001126e34
dungbui2511 <binzno2x@gmail.com>
2024-05-22 09:01:17 +0700

    update detail cart

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -10,15 +10,25 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 class DetailsComponent extends Component
 {
     public $slug;
+    public $qty;
     public function mount($slug)
     {
         $this->slug = $slug;
+        $this->qty = 1;
     }
     public function store($product_id,$product_name,$product_price)
     {
-        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,$this->qty,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added successfully');
         return redirect()->route('product.cart');
+    }
+    public function decreseQuantity(){
+        if($this->qty > 1){
+            $this->qty--;
+        }
+    }
+    public function increaseQuantity(){
+        $this->qty++;
     }
     public function render()
     {
--- resources/views/livewire/details-component.blade.php
+++ resources/views/livewire/details-component.blade.php
@@ -1,4 +1,3 @@
-<!--main area-->
 <main id="main" class="main-site">
     <style>
         .regprice{
@@ -60,9 +59,9 @@
                     <div class="quantity">
                         <span>Quantity:</span>
                         <div class="quantity-input">
-                            <input type="text" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" >
-                            <a class="btn btn-reduce" href="#"></a>
-                            <a class="btn btn-increase" href="#"></a>
+                            <input type="text" name="product-quatity" value="1" data-max="120" pattern="[0-9]*" wire:model="qty">
+                            <a class="btn btn-reduce" href="#" wire:click.prevent="decreseQuantity"></a>
+                            <a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity"></a>
                         </div>
                     </div>
                     <div class="wrap-butons">
@@ -447,4 +446,3 @@
 
 </div><!--end container-->
 </main>
-<!--main area-->
